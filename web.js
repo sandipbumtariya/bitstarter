875c99edde1e76cb48b7ab925e0011bc2a075e0b
Sandip Bumtariya <sandip.bumtariya@gmail.com>
2013-07-18 10:24:13 -0700

    added index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var strn = fs.readFileSync("index.html");
+buf = new Buffer(30);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	response.send(buf.toString('utf8',0, strn));
 });
 
 var port = process.env.PORT || 5000;
