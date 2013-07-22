3713d29d01161d35da72c8a56095609870c90f63
Sandip Bumtariya <sandip.bumtariya@gmail.com>
2013-07-22 07:42:57 -0700

    made hw3 part 3

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var HTMLFILE_DEFAULT = "http://ancient-ocean-6936.herokuapp.com/";
 var CHECKSFILE_DEFAULT = "checks.json";
+var sys = require('util'),
+    rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,12 +46,12 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile, outfile) {
+//    $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present = outfile(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
@@ -64,11 +66,22 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	 .option('-u, --url <check_url>', 'Path to URL http://ancient-ocean-6936.herokuapp.com/')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+
+	if (program.url != null) {
+        rest.get(program.url).on('complete', function(result) {
+            if (result instanceof Error) {
+                sys.puts('Error: ' + result.message);
+                this.retry(5000); // try again after 5 sec
+            } else {
+                fs.writeFileSync('outfile.html', result);
+		  var outfile = cheerio.load(result);
+                var checkJson = checkHtmlFile('outfile.html', program.checks, outfile);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+            }
+        });
+	} }else {
     exports.checkHtmlFile = checkHtmlFile;
 }
